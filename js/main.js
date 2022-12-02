0822c1001b8a2830ad89e81542dbe27b4867738a
Laura <lauradanielapg97@gmail.com>
2022-12-01 20:45:39 -0500

    Trabajando en la logica

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ import { saludo } from "./hellow.js";
 
 const formulario = document.querySelector("form");
 const exportarBoton = document.getElementById("generarPdf");
-const volverBoton = document.getElementById("volver");
 
+const volverBoton = [...document.getElementsByClassName("volver")];
 const contenedorInicio = document.getElementsByClassName("contenedor-inicio");
-const contenedorOpciones = document.getElementsByClassName(
-  "contenedor-opciones"
-);
-const contenedorExportar = document.getElementsByClassName(
-  "contenedor-exportar"
-);
+const contenedorOpciones = document.getElementsByClassName("contenedor-opciones");
+const contenedorExportar = document.getElementsByClassName("contenedor-exportar");
+
+const listHechosEle = [...document.querySelectorAll(".hechos")];
+const listTiposEle = [...document.querySelectorAll(".tipos")];
+const listHechos = ["PROMOVER", "SOLICITAR", "INFORMAR", "RECABAR"];
+const listTipos = ["TIPO 1", "TIPO 2", "TIPO 3", "TIPO 4"];
+const RegexNum = /^[0-9]+$/;
 
 const datos = {
   numCedula: "",
@@ -18,6 +20,39 @@ const datos = {
   hechos: "",
   tipo: "",
 };
+
+listHechosEle?.forEach((ele, i) => {
+  ele.value = listHechos[i];
+  ele.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    datos.hechos = ev.target.value;
+  });
+});
+listTiposEle?.forEach((ele, i) => {
+  ele.value = listTipos[i];
+  ele.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    datos.tipo = ev.target.value;
+    if(datos.hechos.trim() !== ""){
+        setPasoApaso(ev, "none", "none", "flex");
+    }else{
+        errorFaltaDato("contenedor-hechos")
+    }
+  });
+});
+
+function errorFaltaDato (byID) {
+    let restablecer = 1;
+    document.getElementById(byID).style.border = "1px solid #F54A4A";
+    const aviso = setInterval(() => {
+      restablecer--;
+      if (restablecer === 0) {
+        document.getElementById(byID).style.border = "";
+        clearInterval(aviso);
+      }
+    }, 2000);
+}
+
 
 /**
  * @param {ev} ev De donde se activo el evento, event
@@ -29,8 +64,14 @@ function formCampos(ev) {
   const campos = Object.fromEntries(new FormData(ev.target));
   datos.numCedula = campos.numeroCedula;
   datos.nombresApellidos = campos.nombresApellidos;
-  if (datos.numCedula.trim() !== "" && datos.nombresApellidos.trim() !== "") {
+  if (
+    datos.numCedula.trim() !== "" &&
+    RegexNum.test(datos.numCedula.trim()) &&
+    datos.nombresApellidos.trim() !== ""
+  ) {
     setPasoApaso(ev, "none", "block", "none");
+  } else {
+    errorFaltaDato("numeroCedula")
   }
 }
 
@@ -51,9 +92,13 @@ formulario.addEventListener("submit", (ev) => {
   formCampos(ev);
 });
 
-volverBoton.addEventListener("click", (ev) => {
-  setPasoApaso(ev, "block", "none", "none");
-});
+volverBoton.forEach((vol, i)=>{
+    vol.addEventListener("click", (ev) => {
+        i === 0? 
+        setPasoApaso(ev, "block", "none", "none"):
+        setPasoApaso(ev, "none", "block", "none");
+      });
+})
 
 exportarBoton.addEventListener("click", (ev) => {
   var doc = new jsPDF();
